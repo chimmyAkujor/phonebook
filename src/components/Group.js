45925c373d97de24e0398be7136d306db28b12e5
Chimere Akujor <chimere.akujor@mavs.uta.edu>
2018-05-18 03:14:05 -0500

    fixed group and contacts rendering

--- src/components/Group.js
+++ src/components/Group.js
@@ -3,17 +3,17 @@ import Contact from './Contact';
 
 class Group extends Component{
     render() {
-      var subList = [];
-      if(this.props.firstName){
-        var person = {
-          firstName: this.props.firstName,
-          lastName: this.props.lastName,
-          address: ""
-        }
-        subList.push(person);
-        let d = document.getElementsByTagName("div");
-        d.appendChild(<Contact/>);
-      }
+      // var subList = [];
+      // if(this.props.firstName){
+      //   var person = {
+      //     firstName: this.props.firstName,
+      //     lastName: this.props.lastName,
+      //     address: ""
+      //   }
+      //   subList.push(person);
+      //   let d = document.getElementsByTagName("div");
+      //   d.appendChild(<Contact/>);
+      // }
       return (
         <div>
           <h4> {this.props.title} </h4>
--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component,Fragment } from 'react';
 import './List.css';
 import Group from './Group';
 import Contact from './Contact';
 
+var contacts = [
+    {name:"John Deere"},
+    {name:"Jeff Green"},
+    {name: "Nia Long"},
+    {name: "Michelle Bigsby"},
+    {name: "Adrian Young"},
+    {name: "Brian McManaman"}
+].sort(function(a,b) {return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);})
+
 class List extends Component{
+    renderContacts=()=>{
+        let indexes = [];
+        for(var a = 0; a<contacts.length;a++){
+            var index = contacts[a].name[0];
+            if(indexes.indexOf(index)===-1){
+                indexes.push(index);
+            }
+        }
+        return contacts.map((contact,i)=>{
+            const ind = indexes.indexOf(contact.name[0]);
+            if(ind >-1){
+                indexes.splice(ind,1);
+                return (
+                    <Fragment>
+                        <Group key={i} title={contact.name[0]}/>
+                        <Contact key={i} name = {contact.name}/>
+                    </Fragment>
+                )
+            }
+            return <Contact key={i} name = {contact.name}/>
+
+            
+    })
+    }
         render(){
-            const index = [];
+/*            const index = [];
             var charA = 'a', charZ = 'z', i = charA.charCodeAt(0),
             j = charZ.charCodeAt(0);
             for(;i<=j;++i){
@@ -14,10 +47,10 @@ class List extends Component{
             index.push('#');
 
         var list = index.map((title,x)=><Group key={x} title={title}/>);
-        
+  */      
         return(
             <div>
-                {list}
+                {this.renderContacts()}
             </div>
         )
         }
